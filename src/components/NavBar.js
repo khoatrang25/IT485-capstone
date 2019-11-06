bc12614e086584f41e9429ed0ce214178b5e9b6a
Khoa Trang <khoatrang@khoas-mbp.ssw.umb.edu>
2019-11-06 15:51:20 -0500

    percentage edit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,10 +9,6 @@ const NavBar = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-
-  const [show1, setShow1] = useState(false);
-  const handleClose1 = () => setShow1(false);
-  const handleShow1 = () => setShow1(true);
   return (
     <>
       <Modal show={show} onHide={handleClose}>
@@ -33,32 +29,12 @@ const NavBar = () => {
             Login
           </Button></Link>
         </Modal.Footer>
-      </Modal>
-      <Modal show={show1} onHide={handleClose1}>
-        <Modal.Header closeButton1>
-          <Modal.Title>Forgot Your Password?</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p>Enter Your Email</p>
-          <input  className='userAcc' type='email' required pattern='[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2, 4}$'/>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose1}>
-            Cancel
-          </Button>
-          <Button variant="primary" onClick={handleClose1}>
-            Send
-          </Button>
-        </Modal.Footer>
-      </Modal>
+      </Modal>       
     <div>
       <nav>
         <ul>
           <li>
             <Link to="/"><img className="logo" src={logo} alt="logo"></img></Link>
-          </li>
-          <li>
-            <button class="button1" onClick={handleShow1}>Forgot your password?</button>
           </li>
           <br/>
           <li>
